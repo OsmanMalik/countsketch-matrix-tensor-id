846f58246a9bd8a7e37dd52e1e694ae14455d9f2
Osman Malik <osman.malik.87@gmail.com>
2018-12-30 13:11:14 +0100

    Add support for choosing type of QR decomposition for Gaussian tensor ID

--- gaussian_tensor_ID.m
+++ gaussian_tensor_ID.m
@@ -1,7 +1,15 @@
-function Xk = gaussian_tensor_ID(X, k, l)
+function Xk = gaussian_tensor_ID(X, k, l, QR_type)
 %GAUSSIAN_TENSOR_ID Computes Gaussian tensor ID
 %
-%   Xk = GAUSSIAN_TENSOR_ID(X, k, l)
+%   This function requires Tensor Toolbox [1] version 2.6.
+%   
+%   Xk = GAUSSIAN_TENSOR_ID(X, k, l, QR_type) returns a rank-k tensor ID of
+%   the input tensor X computed using an oversampling parameter l-k. Note
+%   that we therefore require l >= k. The computation is done according to
+%   Algorithm 3 of [2]. QR_type controls which type of QR factorization is
+%   used: Set it to 'srrqr' to use the strong rank-revealing QR
+%   factorization of [3] (uses the implementation [4]), or set it to 'qr'
+%   to use Matlab's built-in QR function.
 %
 % REFERENCES:
 %   [1] B. W. Bader, T. G. Kolda and others. MATLAB Tensor Toolbox 
@@ -11,11 +19,17 @@ function Xk = gaussian_tensor_ID(X, k, l)
 %   [2] D. J. Biagioni, D. Beylkin, G. Beylkin. Randomized interpolative
 %       decomposition of separated representations. J. Comput. Phys. 281, 
 %       pp. 116-134, 2015.
+%
+%   [3] M. Gu, and S. C. Eisenstat. Efficient algorithms for computing a
+%       strong rank-revealing QR factorization. SIAM J. Sci. Comput. 17(1),
+%       pp. 848-869, 1996.
+%
+%   [4] X. Xing. Interpolative Decomposition based on Strong RRQR. MATLAB
+%       Central File Exchange. Retrieved November 23, 2018.
 
 N = ndims(X);
 I = size(X);
 R = ncomponents(X);
-f = 2;
 
 % Compute projection Y (Steps 1 and 2 in Alg. 3 of [2])
 Y = ones(l, R);
@@ -26,8 +40,18 @@ end
 Y = repmat(X.lambda.', l, 1) .* Y;
 
 % Compute rank-k matrix ID of Y (Step 3 in Alg. 3 of [2])
-[P, J] = ID(Y.', 'rank', k, f);
-P = P.';
+if strcmp(QR_type, 'srrqr')
+    f = 2;
+    [P, J] = ID(Y.', 'rank', k, f);
+    P = P.';
+elseif strcmp(QR_type, 'qr')
+    [~, R, e] = qr(Y, 0);
+    T = R(1:k, 1:k) \ R(1:k, k+1:end);
+    P = [eye(k) T];
+    pvec(e) = 1:length(e);
+    P = P(:, pvec);
+    J = e(1:k)';
+end
 
 % Form the rank-k tensor ID Xk (Step 4 in Alg. 3 of [2])
 A = cell(N, 1);
