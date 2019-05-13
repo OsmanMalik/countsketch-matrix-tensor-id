2d0b920890b6e9dc9881826f9d051ba2077cb6ea
Osman Malik <osman.malik.87@gmail.com>
2019-05-13 17:07:12 -0600

    Modify scripts for experiment 2 and 3 to allow for a choice between using randomized spectral norm and Matlab's normest when computing errors.

--- run_experiment_2.m
+++ run_experiment_2.m
@@ -75,8 +75,11 @@
 % SRFT_no_splits: Controls how the input matrix is split into pieces in the
 %   execution of SRFT_matrix_ID. This helps avoid exceeding memory usage in
 %   that algorithm when dealing with sparse matrices.
+% norm_type: Set to 'normest' to use Matlab's normest function to compute
+%   error, and to 'normrand' to use randomized spectral norm from [Wo08].
 % no_rand_norm_vec: The number of random vectors used in the computation of
 %   the randomized 2-norm.
+% normest_tol: Used to control tolerance of normest, if normest is used.
 % no_trials: The number of times each experiment is repeated.
 % bin_file: This is the file where the matrix A is stored for input into C
 %   functions
@@ -97,7 +100,9 @@ L = K + 10;
 rho = 5e-3; % rho = 0.01 in experiment 2a, rho = 5e-3 in experiment 2b
 mn = 8;
 SRFT_no_splits = [1 1 1 100 250 500 1000];
+norm_type = 'normest';
 no_rand_norm_vec = 18;
+normest_tol = 1e-2;
 no_trials = 10;
 bin_file = 'data/A_mat.bin';
 results_matlab_file = 'matlab_output';
@@ -200,34 +205,68 @@ for i = 1:length(Is)
             fprintf('Done!\n');
         end
 
-        % Compute errors
+        % If verbose, print which norm is used
         if verbosity >= 1
-            fprintf('Computing errors...\n');
+            if strcmp(norm_type, 'normrand')
+                fprintf('Computing errors using randomized spectral norm...\n');
+            elseif strcmp(norm_type, 'normest')
+                fprintf('Computing errors using normest...\n');
+            else
+                error('Invalid norm type. Exiting.');
+            end
         end
-        X = randn(R, no_rand_norm_vec);
-        X = X./sqrt(sum(X.^2,1));
-        AX = A*X;
+        
+        % If randomized norm is used, compute A*X for random matrix X
+        if strcmp(norm_type, 'normrand')
+            X = randn(R, no_rand_norm_vec);
+            X = X./sqrt(sum(X.^2,1));
+            AX = A*X;
+        end
+        
+        % Compute error for Standard ID
         if I <= I_mem_lim_SID
-            error_SID = max(sqrt(sum(abs(AX - A(:,J_SID)*(P_SID*X)).^2, 1)));
+            if strcmp(norm_type, 'normrand')
+                error_SID = max(sqrt(sum(abs(AX - A(:,J_SID)*(P_SID*X)).^2, 1)));
+            elseif strcmp(norm_type, 'normest')
+                error_SID = normest(A - A(:,J_SID)*P_SID, normest_tol);
+            end
             if verbosity >= 1
                 fprintf('RSVDPACK matrix ID error: %.10e. Time: %.2f s.\n', error_SID, time_SID);
             end
         else
             error_SID = nan;
         end
-        error_GA = max(sqrt(sum(abs(AX - A(:,J_GA)*(P_GA*X)).^2, 1)));
+        
+        % Compute error for Gaussian ID
+        if strcmp(norm_type, 'normrand')
+            error_GA = max(sqrt(sum(abs(AX - A(:,J_GA)*(P_GA*X)).^2, 1)));
+        elseif strcmp(norm_type, 'normest')
+            error_GA = normest(A - A(:,J_GA)*P_GA, normest_tol);
+        end
         if verbosity >= 1
             fprintf('Gaussian matrix ID error: %.10e. Time: %.2f s.\n', error_GA, toc_gaussian);    
         end
+        
+        % Compute error for SRFT ID
         if I <= I_mem_lim_SRFT
-            error_SRFT = max(sqrt(sum(abs(AX - A(:,J_SRFT)*(P_SRFT*X)).^2, 1)));
+            if strcmp(norm_type, 'normrand')
+                error_SRFT = max(sqrt(sum(abs(AX - A(:,J_SRFT)*(P_SRFT*X)).^2, 1)));
+            elseif strcmp(norm_type, 'normest')
+                error_SRFT = normest(A - A(:,J_SRFT)*P_SRFT, normest_tol);
+            end
             if verbosity >= 1
                 fprintf('SRFT matrix ID error: %.10e. Time: %.2f s.\n', error_SRFT, toc_SRFT);
             end
         else
             error_SRFT = nan;
         end
-        error_CS = max(sqrt(sum(abs(AX - A(:,J_CS)*(P_CS*X)).^2, 1)));
+        
+        % Compute error for CS ID
+        if strcmp(norm_type, 'normrand')
+            error_CS = max(sqrt(sum(abs(AX - A(:,J_CS)*(P_CS*X)).^2, 1)));
+        elseif strcmp(norm_type, 'normest')
+            error_CS = normest(A - A(:,J_CS)*P_CS, normest_tol);
+        end
         if verbosity >= 1
             fprintf('CountSketch matrix ID error: %.10e. Time: %.2f s.\n', error_CS, toc_CS);
         end
@@ -249,6 +288,6 @@ for i = 1:length(Is)
         if verbosity >= 1
             fprintf('Done!\n')
         end
-        cnt = cnt + 1;    
+        cnt = cnt + 1;
     end
 end
--- run_experiment_3.m
+++ run_experiment_3.m
@@ -40,8 +40,11 @@
 % SRFT_no_splits: Controls how the input matrix is split into pieces in the
 %   execution of SRFT_matrix_ID. This helps avoid exceeding memory usage in
 %   that algorithm when dealing with sparse matrices.
+% norm_type: Set to 'normest' to use Matlab's normest function to compute
+%   error, and to 'normrand' to use randomized spectral norm from [Wo08].
 % no_rand_norm_vec: The number of random vectors used in the computation of
 %   the randomized 2-norm.
+% normest_tol: Used to control tolerance of normest, if normest is used.
 % no_trials: The number of times each experiment is repeated.
 % filename: This is the location/name of the file where the input data is
 %   stored.
@@ -53,7 +56,9 @@
 K = 1442;
 L = K + 10;
 SRFT_no_splits = 16;
+norm_type = 'normest';
 no_rand_norm_vec = 10;
+normest_tol = 1e-2;
 no_trials = 10;
 filename = '../data/specular.mat';
 results_matlab_file = 'matlab_output_exp_3';
@@ -112,25 +117,50 @@ for tr = 1:no_trials
         fprintf('Done!\n');
     end
     
-    % Compute errors
+    % If verbose, print which norm is used
     if verbosity >= 1
-        fprintf('Computing errors...\n');
+        if strcmp(norm_type, 'normrand')
+            fprintf('Computing errors using randomized spectral norm...\n');
+        elseif strcmp(norm_type, 'normest')
+            fprintf('Computing errors using normest...\n');
+        else
+            error('Invalid norm type. Exiting.');
+        end
     end
-    X = randn(R, no_rand_norm_vec);
-    X = X./sqrt(sum(X.^2,1));
-    AX = A*X;
     
-    error_GA = max(sqrt(sum(abs(AX - A(:,J_GA)*(P_GA*X)).^2, 1)));
+    % If randomized norm is used, compute A*X for random matrix X
+    if strcmp(norm_type, 'normrand')    
+        X = randn(R, no_rand_norm_vec);
+        X = X./sqrt(sum(X.^2,1));
+        AX = A*X;
+    end
+    
+    % Compute error for Gaussian ID
+    if strcmp(norm_type, 'normrand')
+        error_GA = max(sqrt(sum(abs(AX - A(:,J_GA)*(P_GA*X)).^2, 1)));
+    elseif strcmp(norm_type, 'normest')
+        error_GA = normest(A - A(:,J_GA)*P_GA, normest_tol);
+    end
     if verbosity >= 1
         fprintf('Gaussian matrix ID error: %.10e. Time: %.2f s.\n', error_GA, toc_gaussian);    
     end
     
-    error_SRFT = max(sqrt(sum(abs(AX - A(:,J_SRFT)*(P_SRFT*X)).^2, 1)));
+    % Compute error for SRFT ID
+    if strcmp(norm_type, 'normrand')
+        error_SRFT = max(sqrt(sum(abs(AX - A(:,J_SRFT)*(P_SRFT*X)).^2, 1)));
+    elseif strcmp(norm_type, 'normest')
+        error_SRFT = normest(A - A(:,J_SRFT)*P_SRFT, normest_tol);
+    end
     if verbosity >= 1
         fprintf('SRFT matrix ID error: %.10e. Time: %.2f s.\n', error_SRFT, toc_SRFT);
     end
 
-    error_CS = max(sqrt(sum(abs(AX - A(:,J_CS)*(P_CS*X)).^2, 1)));
+    % Compute error for CS ID
+    if strcmp(norm_type, 'normrand')
+        error_CS = max(sqrt(sum(abs(AX - A(:,J_CS)*(P_CS*X)).^2, 1)));
+    elseif strcmp(norm_type, 'normest')
+        error_CS = normest(A - A(:,J_CS)*P_CS, normest_tol);
+    end
     if verbosity >= 1
         fprintf('CountSketch matrix ID error: %.10e. Time: %.2f s.\n', error_CS, toc_CS);
     end
